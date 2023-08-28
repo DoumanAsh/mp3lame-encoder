bcc4d5159582a199413fc1fe717f60d300a9744f
Esther Alter <subalterngames@gmail.com>
2023-08-28 09:49:48 -0400

    x86 conditional compiling (#3)
    Conditional compiling for x86

--- src/input.rs
+++ src/input.rs
@@ -53,7 +53,7 @@ impl EncoderInput for MonoPcm<'_, libc::c_int> {
     }
 }
 
-#[cfg(unix)]
+#[cfg(all(unix, not(target_arch = "x86")))]
 //On most unix it should be i64.
 //But unclear about other platforms, so it is only implemented there as otherwise it is i32.
 impl EncoderInput for MonoPcm<'_, libc::c_long> {
